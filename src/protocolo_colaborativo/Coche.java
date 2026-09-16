a70eede589086347a2dbfc12709f09ff27483752
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-09-15 22:09:42 -0500

    Creacion de clases Coche y Matematicas

---
+++ src/protocolo_colaborativo/Coche.java
@@ -0,0 +1,9 @@
+package protocolo_colaborativo;
+
+public class Coche {
+    private String marca;
+    private String modelo;
+    //private static contadorCoches();
+
+
+}
---
+++ src/protocolo_colaborativo/Matematicas.java
@@ -0,0 +1,9 @@
+package protocolo_colaborativo;
+
+public class Matematicas {
+
+    private int numero1;
+    private int numero2;
+
+
+}
