ccde71cb1a22fd9bc977c348ae7ee342084f7489
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-09-16 20:18:36 -0500

    Creacion de clase para mostrar uso incorrecto de static

---
+++ src/protocolo_colaborativo/StaticIncorrecto.java
@@ -0,0 +1,23 @@
+package protocolo_colaborativo;
+
+public class StaticIncorrecto {
+
+    String atributo;//Atributo NO estatico, variable NO estatica
+
+    public StaticIncorrecto() {//Constructor estandar
+        atributo = "Soy el actual";
+    }
+
+    public static void cambiarAtributo(String nuevoAtributo){//Metodo estatico
+        atributo = nuevoAtributo;
+    }
+
+    public static void main(String[] args) {
+        StaticIncorrecto objeto1 = new StaticIncorrecto();//Instancia de la clase
+        System.out.println(objeto1.atributo);
+
+        cambiarAtributo("Soy el nuevo");//Se llama el metodo estatico
+        System.out.println(objeto1.atributo);
+    }
+
+}
