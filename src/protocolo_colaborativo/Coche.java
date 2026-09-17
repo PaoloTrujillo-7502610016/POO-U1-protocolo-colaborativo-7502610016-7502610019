13f6082120dd49d57621bc90057e1405501b5562
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-09-16 19:53:23 -0500

    Desarrollo de ejercicios clase Matematicas

--- src/protocolo_colaborativo/Coche.java
+++ src/protocolo_colaborativo/Coche.java
@@ -5,13 +5,14 @@ public class Coche {
     private String modelo;
     static int contadorCoches = 0;//Atributo estatico de la clase
 
-    public Coche(String marca, String modelo) {
+    public Coche(String marca, String modelo) {//Constructor parametrizado
         this.marca = marca;
         this.modelo = modelo;
         contadorCoches++;//Incrementa el contador cada vez que se crea un objeto
     }
 
     public static void mostrarContador(){//Metodo para mostrar cuantos coches se han creado
+
         System.out.println("Total de coches creados: " + contadorCoches);
     }
 
--- src/protocolo_colaborativo/Matematicas.java
+++ src/protocolo_colaborativo/Matematicas.java
@@ -2,8 +2,27 @@ package protocolo_colaborativo;
 
 public class Matematicas {
 
-    private int numero1;
-    private int numero2;
+    public static int suma(int numero1, int numero2){
+        return numero1+numero2;
+    }
 
+    public static int resta(int numero1, int numero2){
+        return numero1-numero2;
+    }
+
+    public static int multiplicacion(int numero1, int numero2){
+        return numero1*numero2;
+    }
+
+    public static double division(double numero1, double numero2){
+        return numero1 / numero2;
+    }
+
+    public static void main(String[] args) {//Clase de prueba para llamar los metodos y mostrar los resultados:
+        System.out.println("Suma :"+suma(2,5));
+        System.out.println("Resta :"+resta(2,5));
+        System.out.println("Multiplicaion :"+multiplicacion(2,5));
+        System.out.println("Division :"+division(2,5));
+    }
 
 }
